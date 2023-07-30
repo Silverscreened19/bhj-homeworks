faf4fe35ece82e56d5ab38131ac8b2fed84bd955
Olya Tatianina <olya.tatianina@gmail.com>
2023-07-30 13:15:36 +0400

    todo task solution fixed

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,8 +3,17 @@ const input = document.querySelector('.tasks__input');
 const addButton = document.querySelector('.tasks__add');
 
 
-addButton.addEventListener('click', (e) => {
-    e.preventDefault();
+addButton.addEventListener('click', addTask);
+
+tasks.addEventListener("click", e => {
+    if (e.target.classList.contains("task__remove")) {
+        e.target.parentElement.remove();
+    }
+});
+
+
+function addTask(event) {
+    event.preventDefault();
     if (input.value.trim() !== '') {
         let task = `
         <div class="task">
@@ -13,21 +22,8 @@ addButton.addEventListener('click', (e) => {
             </div>
             <a href="#" class="task__remove">&times;</a>
         </div>
-        `
-        tasks.insertAdjacentHTML('afterend', task);
+        `;
+        tasks.insertAdjacentHTML('beforeend', task);
         input.value = '';
     }
-    removeTask();
-})
-
-
-function removeTask() {
-    const taskRemove = Array.from(document.querySelectorAll('.task__remove'))
-    taskRemove.forEach((item) => {
-        item.addEventListener('click', (e) => {
-            e.preventDefault();
-            activeTask = item.closest('.task');
-            activeTask.remove();
-        })
-    })
 }
