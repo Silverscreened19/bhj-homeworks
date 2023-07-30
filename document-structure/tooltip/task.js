ca5526062b99146a50d307382be7958559e53afb
Olya Tatianina <olya.tatianina@gmail.com>
2023-07-30 12:30:16 +0400

    tooltip task solution fixed

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,18 +1,24 @@
-const links = Array.from(document.querySelectorAll('.has-tooltip'))
-const tip = document.createElement('div')
-tip.classList.add('tooltip')
-tip.setAttribute('data-position', 'bottom')
+const links = Array.from(document.querySelectorAll('.has-tooltip'));
+const tip = document.createElement('div');
+tip.classList.add('tooltip');
+tip.setAttribute('data-position', 'bottom');
+
 
 links.forEach(link => {
     link.addEventListener('click', (e) => {
-        e.preventDefault()
-        link.insertAdjacentElement('afterend', tip)
-        tip.innerText = `${link.title}`
-        tip.classList.add('tooltip_active')
+        e.preventDefault();
+        let activeTip = document.querySelector('.tooltip_active');
+        if (activeTip == null || link.nextSibling.className != 'tooltip tooltip_active') {
+            link.insertAdjacentElement('afterend', tip);
+            tip.innerText = `${link.title}`;
+            tip.classList.add('tooltip_active');
 
-        link.style.position = 'relative'
-        tip.style.position = 'absolute'
+            link.style.position = 'relative';
+            tip.style.position = 'absolute';
 
-        tip.style.left = `${link.offsetLeft}px`;
+            tip.style.left = `${link.offsetLeft}px`;
+        } else {
+            link.nextSibling.remove();
+        }
     })
 })
